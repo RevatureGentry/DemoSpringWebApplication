a7ba1b132251d30a40347a7ed2af4cd58607e306
RevatureGentry <william.gentry@revature.com>
2019-12-04 11:45:59 -0500

    Fixed faulty registration controller

--- src/main/java/com/revature/controller/RegistrationController.java
+++ src/main/java/com/revature/controller/RegistrationController.java
@@ -46,7 +46,7 @@ public class RegistrationController {
 		}
 		if (service.attemptRegisterUser(form) != null) { 
 			model.addAttribute("registrationSuccessMessage", form.getUsername() + " successfully registered!");
-			return "/login";
+			return "login";
 		}
 		return "register";
 	}
